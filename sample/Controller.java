47e10477f56ce0b496cac6205c38229ea2d40592
KArthik K Devadiga <kdevadiga497@gmail.com>
2021-08-24 20:55:53 +0530

    added function to hand-burger button

--- sample/Controller.java
+++ sample/Controller.java
@@ -1,25 +1,50 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+
 public class Controller {
     @FXML
+    private VBox vbox;
+
+    @FXML
     public void initialize(){
-        getFonts();
+        vbox.setVisible(false);
     }
 
-    public void getFonts(){
-        List<String> list =  Font.getFamilies();
-        for(String fint : list){
-            System.out.println(fint);
+    //hand-burger
+    public void showSideBar(ActionEvent e){
+        if(vbox.isVisible()){
+            vbox.setVisible(false);
+        }else{
+            vbox.setVisible(true);
         }
+
     }
+
+
+
+
 
 }
 
 
 //
+
+
+
+/* public void getFonts(){
+ List<String> list =  Font.getFamilies();
+ for(String fint : list){
+ System.out.println(fint);
+ }
+ }
+ */
